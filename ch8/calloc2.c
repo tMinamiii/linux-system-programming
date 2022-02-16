26966b352a735afb75d5c32eb7806fbaac1d9012
tMinamiii <otherthan.new@gmail.com>
2022-02-16 11:50:46 +0900

    malloc

--- ch8/calloc2.c
+++ ch8/calloc2.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-       int *pMMem; // malloc用
-       int *pCMem; // calloc用
+int main(void) {
+  int *pMMem; // malloc用
+  int *pCMem; // calloc用
 
-       pMMem = (int *)malloc(sizeof(int) * 3);
-       pCMem = (int *)calloc(3, sizeof(int));
+  pMMem = (int *)malloc(sizeof(int) * 1024);
+  pCMem = (int *)calloc(1024, sizeof(int));
 
-       //確保領域の初期値表示
-       printf("malloc [0]:0x%08x, [1]:0x%08x, [2]:0x%08x\n", pMMem[0], pMMem[1],
-              pMMem[2]);
-       printf("calloc [0]:0x%08x, [1]:0x%08x, [2]:0x%08x\n", pCMem[0], pCMem[1],
-              pCMem[2]);
+  //確保領域の初期値表示
+  printf("malloc [0]:0x%08x, [1]:0x%08x, [2]:0x%08x\n", pMMem[100], pMMem[101],
+         pMMem[102]);
+  printf("calloc [0]:0x%08x, [1]:0x%08x, [2]:0x%08x\n", pCMem[100], pCMem[101],
+         pCMem[102]);
 
-       free(pMMem);
-       free(pCMem);
+  free(pMMem);
+  free(pCMem);
 
-       return 0;
+  return 0;
 }
--- ch8/segment.c
+++ ch8/segment.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 
 int main() {
+  // heap ?
   printf("The current break point is %p\n", sbrk(0));
   printf("Increment 1 break point is %p\n", sbrk(1));
   printf("The current break point is %p\n", sbrk(0));
